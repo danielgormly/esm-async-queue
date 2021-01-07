09d837340c7e7d3b1b7626d3d095609edcfb5325
Daniel Gormly <danielgormly@daniels-macbook.home>
2021-01-07 13:27:53 +1100

    Updated test to check taskProcessed event

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,11 +3,15 @@ import { Queue } from './index.js';
 import { delayedResolution } from './helpers.js';
 
 test('Rudimentary test', (t) => {
+  t.plan(5);
   const queue = new Queue({ concurrency: 2 });
   queue.push(() => delayedResolution(200));
   queue.push(() => delayedResolution(200));
   queue.push(() => delayedResolution(200));
   queue.push(() => delayedResolution(200));
+  queue.subscribe('taskProcessed', () => {
+    t.true(true, 'taskProcessed event captured');
+  });
   queue.subscribe('idle', () => {
     t.true(true, 'idle event captured');
     t.end();
